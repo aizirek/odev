fe5f64f33176ab6986006d340a5e25c0ecc0a2b7
aizirek <ayzirek2295@mail.ru>
2016-03-17 22:55:39 +0600

    1

---
+++ world.java
@@ -0,0 +1,15 @@
+package world.com;
+
+/**
+ * Created by begmatova on 10.03.16.
+ */
+public class world {
+    public  static void main(String[] args){
+        System.err.println("salam");
+        
+
+
+
+
+    }
+}
