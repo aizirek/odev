3c419893596c08bf985f5cfe3fd642988ecc2498
aizirek <ayzirek2295@mail.ru>
2016-03-17 23:28:25 +0600

    4

--- world.java
+++ world.java
@@ -7,19 +7,7 @@ public class world {
     public  static void main(String[] args){
         System.err.println("salam");
         System.err.println("Привет");
-        System.err.println("Merhaba");
-        System.err.println("Hello");
-
-        System.err.println("salam");
-        System.err.println("Привет");
-        System.err.println("Merhaba");
-        System.err.println("Hello");
-
-        System.err.println("salam");
-        System.err.println("Привет");
-        System.err.println("Merhaba");
-        System.err.println("Hello");
-
+    
 
 
 
