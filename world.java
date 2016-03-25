bb2a202e64260523afd8fc6f412fddd0053c2cb1
aizirek <ayzirek2295@mail.ru>
2016-03-25 09:58:01 +0600

    7

--- world.java
+++ world.java
@@ -5,19 +5,8 @@ package world.com;
  */
 public class world {
     public  static void main(String[] args){
-        System.err.println("1");
-        System.err.println("2");
-        System.err.println("3");
-        System.err.println("4");
-        System.err.println("5");
-        System.err.println("6");
-        System.err.println("7");
-
-
-
-
-
-
+        System.err.println("7 commit");
+    
 
     }
 }
